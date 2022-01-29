8a283f250d0d420120ffdc40d8f579bc92713237
Brandon Eng <brandoneng000@gmail.com>
2022-01-29 02:21:02 -0800

    Added monster hit zone values(hzv) to model

--- MHR_monster_data/monster_api/models.py
+++ MHR_monster_data/monster_api/models.py
@@ -8,3 +8,22 @@ class Monster(models.Model):
 
     def __str__(self) -> str:
         return self.monster_name
+
+class Monster_HZV(models.Model):
+    part_id = models.IntegerField()
+    monster_id = models.ForeignKey(Monster, on_delete=models.CASCADE, blank=False)
+    part_name = models.CharField(max_length=50)
+    state = models.IntegerField()
+    sever = models.IntegerField()
+    impact  = models.IntegerField()
+    ammo = models.IntegerField()
+    fire = models.IntegerField()
+    water = models.IntegerField()
+    thunder = models.IntegerField()
+    ice = models.IntegerField()
+    dragon = models.IntegerField()
+    stun = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.part_id},{self.monster_id},{self.part_name},{self.state},{self.sever},{self.impact},{self.ammo},{self.fire},"\
+                 + f"{self.water},{self.ice},{self.thunder},{self.dragon},{self.stun}"
--- MHR_monster_data/monster_api/serializers.py
+++ MHR_monster_data/monster_api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
-from .models import Monster
+from .models import Monster, Monster_HZV
 
 class MonsterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Monster
         fields = ('id', 'monster_name')
+
+class MonsterHZVSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Monster_HZV
+        fields = '__all__'
