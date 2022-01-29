4a6daea032fc0fcde3c6f6d98584198269a5ab0c
Brandon Eng <brandoneng000@gmail.com>
2022-01-29 06:47:59 -0800

    Fixed security issue

--- MHR_monster_data/MHR_monster_data/settings.py
+++ MHR_monster_data/MHR_monster_data/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!wh*-z%l@v6noe8=7^4o5(s9)bklz6ojr1v*wynl%zuw8rz&hd'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-               'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+               'read_default_file': os.path.join(BASE_DIR, '.env'),
             }
     }
 }
