b7607dcf3404ae511f2da90ad6b6d15ba7a70639
Brandon Eng <brandoneng000@gmail.com>
2022-01-29 02:07:42 -0800

    Worked on Monster model and URLs

--- MHR_monster_data/monster_api/views.py
+++ MHR_monster_data/monster_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import MonsterSerializer
+from .models import Monster
 
 # Create your views here.
+class MonsterViewSet(viewsets.ModelViewSet):
+    queryset = Monster.objects.all()
+    serializer_class = MonsterSerializer
