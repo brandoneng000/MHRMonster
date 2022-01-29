88ec0820e38e39318182147d34a24bcea5fa03c4
Brandon Eng <brandoneng000@gmail.com>
2022-01-29 01:27:15 -0800

    Made monster model

--- MHR_monster_data/monster_api/models.py
+++ MHR_monster_data/monster_api/models.py
@@ -1,3 +1,10 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+class Monster(models.Model):
+    id = models.IntegerField(primary_key=TRUE)
+    monster_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.monster_name
