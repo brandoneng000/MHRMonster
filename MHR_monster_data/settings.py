89dd901d16547a6077170208fa01648acb3473aa
Brandon Eng <brandoneng000@gmail.com>
2022-02-01 23:29:13 -0800

    Adjusting static directory

--- MHR_monster_data/settings.py
+++ MHR_monster_data/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/MHR_monster_data/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/MHR_monster_data/static/'
 
 STATICFILES_DIRS = [
