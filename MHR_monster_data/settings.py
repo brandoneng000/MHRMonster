aa2c79052590fc01f054448ddf7691f71bae42c8
Brandon Eng <brandoneng000@gmail.com>
2022-02-01 22:43:07 -0800

    Changed static

--- MHR_monster_data/settings.py
+++ MHR_monster_data/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/MHR_monster_data/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "MHR_monster_data" /"static",
