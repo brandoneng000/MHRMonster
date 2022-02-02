e8540a50a4a820d2e56b70184ef71574bbc6b3f8
Brandon Eng <brandoneng000@gmail.com>
2022-02-01 22:53:56 -0800

    Testing static

--- MHR_monster_data/settings.py
+++ MHR_monster_data/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "MHR_monster_data" /"static",
+    BASE_DIR /"static",
 ]
 
 # Default primary key field type
