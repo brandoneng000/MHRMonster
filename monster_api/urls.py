424eab5a05680c47d7da47017b9d2d7c8c4a1c67
Brandon Eng <brandoneng000@gmail.com>
2022-02-11 23:03:12 -0800

    Changed router

--- monster_api/urls.py
+++ monster_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'monster', views.MonsterViewSet, basename="monster")
 
 app_name = 'monster'
